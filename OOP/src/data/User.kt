3788d2ed7a64842d1442cb085480e73cc171658e
artharestu <artharestuabadi@gmail.com>
2024-05-21 16:05:24 +0700

    Properties Constructor

--- OOP/src/data/User.kt
+++ OOP/src/data/User.kt
@@ -1,5 +1,7 @@
 package data
 
+// Properti dalam constructor adalah cara untuk menginisialisasi nilai properti objek saat pertama kali
+// dibuat. Properti ini didefinisikan di dalam constructor dan langsung diberi nilai awal.
 class User(var username: String, var password: String) {
 
     override fun toString(): String {
